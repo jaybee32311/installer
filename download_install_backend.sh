ea8cfdb5d19c217b25e1776b73787b12a21f63f6
jaybee32311 <121048462+jaybee32311@users.noreply.github.com>
2022-12-26 06:17:44 -0800

    Add files via upload

---
+++ download_install_backend.sh
@@ -0,0 +1,5 @@
+git clone https://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@github.com/jaybee32311/srvhst_linux_backend.git
+cd srvhst_linux_backend
+chmod 777 install_backend.sh
+./install_backend.sh
+
